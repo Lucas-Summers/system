d58f1b54d76e6c05f4ce6b0db923fa2ea64f05fe
Lucas Summers <lucas.summers.dev@gmail.com>
2026-04-03 13:19:03 -0700

    added new host to secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
+  m1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMgXB7v8UmgXtvespZsO7zmDfL+7iBiMgKVjAtLswNYd lucas.summers.dev@gmail.com";
   m5 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJmnvWIaW7vq5vxHzgRjN0p6DY50COjl+06q8+QP3NEH lucas.summers.dev@gmail.com";
 in {
-  "gpg_priv.asc.age".publicKeys = [ m5 ];
-  "gpg_pub.asc.age".publicKeys  = [ m5 ];
+  "gpg_priv.asc.age".publicKeys = [ m1 m5 ];
+  "gpg_pub.asc.age".publicKeys  = [ m1 m5 ];
 }
